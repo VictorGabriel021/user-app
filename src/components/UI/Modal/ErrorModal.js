c72977718870c531fa46cc145363709998ab839a
VictorGabriel021 <victorgabriel.cipa@gmail.com>
2021-12-21 08:19:00 -0300

    refactor: Adding react portal in ErrorModal

---
+++ src/components/UI/Modal/ErrorModal.js
@@ -0,0 +1,46 @@
+import Button from "../Button";
+import Card from "../Card";
+import styles from "./ErrorModal.module.css";
+import ReactDOM from "react-dom";
+import React from "react";
+
+const BackDrop = (props) => {
+  return <div className={styles.backdrop} onClick={props.onConfirm}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <Card className={styles.modal}>
+      <header className={styles.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={styles.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={styles.actions}>
+        <Button onClick={props.onConfirm}>Fechar</Button>
+      </footer>
+    </Card>
+  );
+};
+
+const ErrorModal = (props) => {
+  return (
+    <React.Fragment>
+      {ReactDOM.createPortal(
+        <BackDrop onConfirm={props.onConfirm} />,
+        document.getElementById("backdrop-root")
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          onConfirm={props.onConfirm}
+        />,
+        document.getElementById("overlay-root")
+      )}
+    </React.Fragment>
+  );
+};
+
+export default ErrorModal;
